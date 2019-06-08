3090e7de51a21cc02f3617f7aca21a1ca01a5e00
Stephane Larocque <stephl001@gmail.com>
2019-06-08 09:46:10 -0400

    Refactor mappings

--- src/Mappings.elm
+++ src/Mappings.elm
@@ -1,4 +1,4 @@
-module Mappings exposing (Mappings, commonMappings, mapLetter, mappingsFromString, reverseMappings)
+module Mappings exposing (CommonMappings(..), Mappings, ReflectorMappings(..), RotorMappings(..), mapLetter, mappingsFromString, toMappings)
 
 import Array exposing (Array)
 import Array.Extra as Array
@@ -8,6 +8,80 @@ import Set exposing (fromList)
 
 type Mappings
     = Mappings (Array Letter)
+    | StringMappings String
+
+
+type CommonMappings
+    = Identity
+    | Rotor RotorMappings
+    | Reflector ReflectorMappings
+
+
+type RotorMappings
+    = RotorI
+    | RotorII
+    | RotorIII
+    | RotorIV
+    | RotorV
+    | RotorVI
+    | RotorVII
+    | RotorVIII
+
+
+type ReflectorMappings
+    = ReflectorA
+    | ReflectorB
+    | ReflectorC
+
+
+toMappings : CommonMappings -> Mappings
+toMappings mappingId =
+    case mappingId of
+        Identity ->
+            StringMappings "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        Rotor RotorI ->
+            StringMappings "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+
+        Rotor RotorII ->
+            StringMappings "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+
+        Rotor RotorIII ->
+            StringMappings "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+
+        Rotor RotorIV ->
+            StringMappings "ESOVPZJAYQUIRHXLNFTGKDCMWB"
+
+        Rotor RotorV ->
+            StringMappings "VZBRGITYUPSDNHLXAWMJQOFECK"
+
+        Rotor RotorVI ->
+            StringMappings "JPGVOUMFYQBENHZRDKASXLICTW"
+
+        Rotor RotorVII ->
+            StringMappings "NZJHGRCXMYSWBOUFAIVLPEKQDT"
+
+        Rotor RotorVIII ->
+            StringMappings "FKQHTLXOCBJSPDZRAMEWNIUYGV"
+
+        Reflector ReflectorA ->
+            StringMappings "EJMZALYXVBWFCRQUONTSPIKHGD"
+
+        Reflector ReflectorB ->
+            StringMappings "YRUHQSLDPXNGOKMIEBFZCWVJAT"
+
+        Reflector reflectorCMappings ->
+            StringMappings "FVPJIAOYEDRZXWGCTKUQSBNMHL"
+
+
+toLetterArray : Mappings -> Array Letter
+toLetterArray mappings =
+    case mappings of
+        Mappings letters ->
+            letters
+
+        StringMappings letters ->
+            toLetterList letters |> Array.fromList
 
 
 mappingsFromLetters : LetterList -> Maybe Mappings
@@ -21,126 +95,7 @@ mappingsFromString =
 
 
 
---Do not export mappingsFromList. Internal helper only.
-
-
-mappingsFromList : LetterList -> Mappings
-mappingsFromList =
-    Array.fromList >> Mappings
-
-
-
 {- Known Mappings from documentation -}
-
-
-type alias CommonMappings =
-    { identity : Mappings
-    , rotorI : Mappings
-    , rotorII : Mappings
-    , rotorIII : Mappings
-    , rotorIV : Mappings
-    , rotorV : Mappings
-    , rotorVI : Mappings
-    , rotorVII : Mappings
-    , rotorVIII : Mappings
-    , reflectorA : Mappings
-    , reflectorB : Mappings
-    , reflectorC : Mappings
-    }
-
-
-identityMappings : Mappings
-identityMappings =
-    [ A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z ]
-        |> mappingsFromList
-
-
-rotorIMappings : Mappings
-rotorIMappings =
-    [ E, K, M, F, L, G, D, Q, V, Z, N, T, O, W, Y, H, X, U, S, P, A, I, B, R, C, J ]
-        |> mappingsFromList
-
-
-rotorIIMappings : Mappings
-rotorIIMappings =
-    [ A, J, D, K, S, I, R, U, X, B, L, H, W, T, M, C, Q, G, Z, N, P, Y, F, V, O, E ]
-        |> mappingsFromList
-
-
-rotorIIIMappings : Mappings
-rotorIIIMappings =
-    [ B, D, F, H, J, L, C, P, R, T, X, V, Z, N, Y, E, I, W, G, A, K, M, U, S, Q, O ]
-        |> mappingsFromList
-
-
-rotorIVMappings : Mappings
-rotorIVMappings =
-    [ E, S, O, V, P, Z, J, A, Y, Q, U, I, R, H, X, L, N, F, T, G, K, D, C, M, W, B ]
-        |> mappingsFromList
-
-
-rotorVMappings : Mappings
-rotorVMappings =
-    [ V, Z, B, R, G, I, T, Y, U, P, S, D, N, H, L, X, A, W, M, J, Q, O, F, E, C, K ]
-        |> mappingsFromList
-
-
-rotorVIMappings : Mappings
-rotorVIMappings =
-    [ J, P, G, V, O, U, M, F, Y, Q, B, E, N, H, Z, R, D, K, A, S, X, L, I, C, T, W ]
-        |> mappingsFromList
-
-
-rotorVIIMappings : Mappings
-rotorVIIMappings =
-    [ N, Z, J, H, G, R, C, X, M, Y, S, W, B, O, U, F, A, I, V, L, P, E, K, Q, D, T ]
-        |> mappingsFromList
-
-
-rotorVIIIMappings : Mappings
-rotorVIIIMappings =
-    [ F, K, Q, H, T, L, X, O, C, B, J, S, P, D, Z, R, A, M, E, W, N, I, U, Y, G, V ]
-        |> mappingsFromList
-
-
-reflectorAMappings : Mappings
-reflectorAMappings =
-    [ E, J, M, Z, A, L, Y, X, V, B, W, F, C, R, Q, U, O, N, T, S, P, I, K, H, G, D ]
-        |> mappingsFromList
-
-
-reflectorBMappings : Mappings
-reflectorBMappings =
-    [ Y, R, U, H, Q, S, L, D, P, X, N, G, O, K, M, I, E, B, F, Z, C, W, V, J, A, T ]
-        |> mappingsFromList
-
-
-reflectorCMappings : Mappings
-reflectorCMappings =
-    [ F, V, P, J, I, A, O, Y, E, D, R, Z, X, W, G, C, T, K, U, Q, S, B, N, M, H, L ]
-        |> mappingsFromList
-
-
-commonMappings : CommonMappings
-commonMappings =
-    { identity = identityMappings
-    , rotorI = rotorIMappings
-    , rotorII = rotorIIMappings
-    , rotorIII = rotorIIIMappings
-    , rotorIV = rotorIVMappings
-    , rotorV = rotorVMappings
-    , rotorVI = rotorVIMappings
-    , rotorVII = rotorVIIMappings
-    , rotorVIII = rotorVIIIMappings
-    , reflectorA = reflectorAMappings
-    , reflectorB = reflectorBMappings
-    , reflectorC = reflectorCMappings
-    }
-
-
-reverseMappings : Mappings -> Mappings
-reverseMappings mappings =
-    mappings
 
 
 mapLetter : Mappings -> Letter -> Letter
